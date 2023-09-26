0f0422aa7e02bd5e344bf4c7702f80e45b1de0fc
ybarakat22 <ybarakat@identity-solutions.org>
2023-09-26 20:43:43 +0300

    Add alert after submiiting report seller

--- src/components/ReportSellerWindow.tsx
+++ src/components/ReportSellerWindow.tsx
@@ -1,47 +1,41 @@
-import React, { useState } from "react";
+import React, { ReactEventHandler, useState } from "react";
 const ReportSeller = () => {
-  const windowBody: string = `<html>
+  const handleSubmit:ReactEventHandler= ()=>{
+    
+
+  }
+  const windowBody: string = `
+<html>
   <body>
+    <script>
+     function handleSubmit(){
+      window.close();
+      alert("Your report has been sent!\\nWe’ve sent your report to our moderators.Thank you for keeping LAGAIT safe!")
+    }
+    </script>
     <div class="dialog-content">
-    <h2 style="text-align: center ; font-family: Futura PT;
-    font-size: 64px;
-    font-weight: 500;
-    line-height: 82px;
-    letter-spacing: 0em;
-    text-align: center;
-    ">Report Seller</h2>
-    <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left; margin-bottom:66px;">
-    <input type="checkbox" class="dialog-checkbox"  /> Spam (Ads and Self Promotion)
-    </div>
-    <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">
-      <input type="checkbox" class="dialog-checkbox" /> Offensive, obsecene messages
-    </div>
-    <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">
-      <input type="checkbox" class="dialog-checkbox" /> Scamming or trolling
-    </div>
-    <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">     
-    <input type="checkbox" class="dialog-checkbox" value="option2" /> Hate speech
-    </div>
-    <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">     
-    <input type="checkbox" class="dialog-checkbox" /> Other
-  </div>
-    <div style="font-family: Poppins;
-    font-size: 24px;
-    font-weight: 400;
-    line-height: 24px;
-    letter-spacing: 0em;
-    text-align: left;
-    margin-bottom:25px;" >Tell me more about this</div>
-    <input style="width: 791.93px;
-    height: 195px;
-    top: 1520px;
-    left: 545.07px;
-    border-radius: 6px;
-    border: 2px;
-    margin-bottom:20px
-    "type="text"></input>
-    </div>
-    <button class="dialog-button" onclick="window.close()">Submit</button>
+      <h2 style="text-align: center ; font-family: Futura PT;font-size: 64px;font-weight: 500;line-height: 82px;letter-spacing: 0em;text-align: center;">Report Seller</h2>
+      <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left; margin-bottom:66px;">
+        <input type="checkbox" class="dialog-checkbox"  /> Spam (Ads and Self Promotion)
+      </div>
+      <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">
+        <input type="checkbox" class="dialog-checkbox" /> Offensive, obsecene messages
+      </div>
+      <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">
+        <input type="checkbox" class="dialog-checkbox" /> Scamming or trolling
+      </div>
+      <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">     
+        <input type="checkbox" class="dialog-checkbox" value="option2" /> Hate speech
+      </div>
+      <div style="font-family: 'Poppins'; font-size: 40px; font-weight: 400; line-height: 60px; letter-spacing: 0em; text-align: left;margin-bottom:66px;">     
+        <input type="checkbox" class="dialog-checkbox" /> Other
+      </div>
+      <div style="font-family: Poppins;font-size: 24px;font-weight: 400;line-height: 24px; letter-spacing: 0em;text-align: left;
+      margin-bottom:25px;" >Tell me more about this
+      </div>
+        <input style="width: 791.93px;height: 195px;top: 1520px;left: 545.07px;border-radius: 6px;border: 2px;margin-bottom:20px"type="text"></input>
+      </div>
+    <button class="dialog-button" onclick="handleSubmit()">Submit</button>
   </body>
 </html>`;
   const openDialog = () => {
